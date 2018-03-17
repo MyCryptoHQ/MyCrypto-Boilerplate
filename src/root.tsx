a6e126eaaba09ea51ef1ab14de3adf0882068eda
james-prado <jamesprado7@gmail.com>
2018-03-17 03:34:46 -0400

    Update prettier rules

--- src/root.tsx
+++ src/root.tsx
@@ -1,8 +1,8 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import 'what-input';
-import App from './app';
-import './sass/styles.scss';
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import 'what-input'
+import App from './app'
+import './sass/styles.scss'
 
-const root = document.getElementById('root');
-ReactDOM.render(<App />, root);
+const root = document.getElementById('root')
+ReactDOM.render(<App />, root)
